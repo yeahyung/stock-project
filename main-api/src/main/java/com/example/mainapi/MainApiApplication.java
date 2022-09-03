f06b66ee7cdaf32b4fbc19afe4a126054702e627
yeahyung <yhb77888@naver.com>
2022-09-03 14:09:31 +0900

    initial commit

---
+++ main-api/src/main/java/com/example/mainapi/MainApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApiApplication.class, args);
+    }
+
+}
